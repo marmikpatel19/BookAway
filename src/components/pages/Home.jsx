4948a09a229b1ebb1c5da976987b344922d48a18
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-01-07 11:59:37 -0700

    develop card component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../../App.css";
 import HeroSection from "../HeroSection";
 import Navbar from "../navbar";
+import Cards from "../Cards";
 
 function Hemo() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Cards />
     </>
   );
 }
