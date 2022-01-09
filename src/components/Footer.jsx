592cd1f861122c7d58aee0e73983941c7549d371
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-01-08 20:55:57 -0700

    refactor footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,120 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Button } from "./Button";
 import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer-container">
-      <section className="footer-subscription">
-        <p className="footer-subscription-heading">
-          Join the Adventure newsletter to receive our best vacation deals
+      <section className="footer-newsletter">
+        <p className="footer-newsletter-heading">
+          Join our un-BEE-lievable newsletter to receive updates about our
+          beehives
         </p>
-        <p className="footer-subscription-text">
-          You can unsubscribe at any time.
-        </p>
-        <div className="input-areas">
+        <div>
           <form>
             <input
               type="email"
               name="email"
-              placeholder="Your Email"
+              placeholder="Email"
               className="footer-input"
             />
-            <Button buttonStyle="btn--outline">Subscribe</Button>
+            <input type="submit" value="Submit" className="footer-submit-btn" />
           </form>
         </div>
       </section>
-      <div class="footer-links">
-        <div className="footer-link-wrapper">
-          <div class="footer-link-items">
-            <h2>About Us</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
-            <Link to="/">Terms of Service</Link>
-          </div>
-          <div class="footer-link-items">
-            <h2>Contact Us</h2>
-            <Link to="/">Contact</Link>
-            <Link to="/">Support</Link>
-            <Link to="/">Destinations</Link>
-            <Link to="/">Sponsorships</Link>
-          </div>
-        </div>
-        <div className="footer-link-wrapper">
-          <div class="footer-link-items">
-            <h2>Videos</h2>
-            <Link to="/">Submit Video</Link>
-            <Link to="/">Ambassadors</Link>
-            <Link to="/">Agency</Link>
-            <Link to="/">Influencer</Link>
-          </div>
-          <div class="footer-link-items">
-            <h2>Social Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-            <Link to="/">Youtube</Link>
-            <Link to="/">Twitter</Link>
-          </div>
-        </div>
-      </div>
-      <section className="social-media">
-        <div className="social-media-wrap">
-          <div className="footer-logo">
-            <Link to="/" className="footer-logo">
-              BookBees
-            </Link>
-            <img
-              src={require("../logo.png")}
-              alt="BookBees logo"
-              className="logo"
-              width="35px"
-            />
-          </div>
-          <small className="website-rights">BookBees © 2022</small>
-          <div className="social-iconrs">
-            <Link
-              className="social-icon-link facebook"
-              to="/"
-              target="_blank"
-              aria-label="Facebook"
-            >
+      <section className="social">
+        <div className="social-wrap">
+          <small className="BookBees-copyright">BookBees © 2022</small>
+          <div className="social-icons">
+            <a href="https://facebook.com" className="social-icon-link">
               <i className="fab fa-facebook-f"></i>
-            </Link>
-            <Link
-              className="social-icon-link instrgram"
-              to="/"
-              target="_blank"
-              aria-label="Instagram"
-            >
+            </a>
+            <a href="https://instagram.com" className="social-icon-link">
               <i className="fab fa-instagram"></i>
-            </Link>
-            <Link
-              class="social-icon-link youtube"
-              to="/"
-              target="_blank"
-              aria-label="Youtube"
-            >
-              <i class="fab fa-youtube" />
-            </Link>
-            <Link
-              class="social-icon-link twitter"
-              to="/"
-              target="_blank"
-              aria-label="Twitter"
-            >
+            </a>
+            <a href="https://twitter.com" className="social-icon-link">
               <i class="fab fa-twitter" />
-            </Link>
-            <Link
-              class="social-icon-link linkedin"
-              to="/"
-              target="_blank"
-              aria-label="LinkedIn"
-            >
-              <i class="fab fa-linkedin" />
-            </Link>
+              <i />
+            </a>
           </div>
         </div>
       </section>
