a95aa6a1d4d8d11dcff8aef18dc8d2de38d2239a
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-01-13 09:04:22 -0700

    implement register page through MUI sign-up template

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import "../../App.css";
 import Footer from "../Footer";
 import Navbar from "../navbar";
+import "./Register.css";
+import SignUp from "./SignUp";
 
 function Register() {
   return (
     <>
       <Navbar />
-      <h1 className="register">REGISTER</h1>
-      <Footer />
+      <h1 className="register-heading">REGISTER</h1>
+      <SignUp />
+      <Footer className="footer" />
     </>
   );
 }
